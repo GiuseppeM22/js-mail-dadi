f9ed5e09a121087b10ff3042563b4d0e1700f9d9
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-18 15:59:12 +0200

    feat: add  codice base email

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const invitati = ["giuseppe@gmail", "alessando@llibero", "mario@hotmail", "siamone@gmail", "marco@libero"]
+console.log (invitati)
+let controllo = prompt("inserire email di invito")
+let messaggio;
+
+for (let i = 0; i < invitati.length; i++) {
+    const invito = invitati[i];
+    
+    if(controllo == invito){
+        messaggio = "benvenuto"
+        document.getElementById("invito").innerText = messaggio
+    }else if (controllo!= invito){
+        messaggio = "Il suo invito non è valido"
+        document.getElementById("invito").innerText = messaggio
+    }
+}
+
