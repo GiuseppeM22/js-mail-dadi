7976ccbaa2ba3b93a98085eda4e9f3a677011408
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-18 16:57:06 +0200

    feat:add break

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ console.log (invitati)
 let controllo = prompt("inserire email di invito")
 let messaggio;
 
-for (let i = 0; i < invitati.length; i++) {
+
+for (let i = 0; i < invitati.length; ++i) {
     const invito = invitati[i];
     
     if(controllo == invito){
         messaggio = "benvenuto"
         document.getElementById("invito").innerText = messaggio
+        console.log(messaggio)
+        break;
+
     }else if (controllo!= invito){
         messaggio = "Il suo invito non è valido"
         document.getElementById("invito").innerText = messaggio
+        console.log(messaggio)
+
     }
+
 }
 
